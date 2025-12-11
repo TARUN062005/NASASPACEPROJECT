380fed693e2a7dd24be176c03cfa47fdd794a278
TARUN062005 <princetarunvemuri@gmail.com>
2025-12-11 21:17:01 +0530

    Updated

--- server/index.js
+++ server/index.js
@@ -100,7 +100,7 @@ app.get("/api/info", (req, res) => {
 });
 
 // All other GET requests return React app
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
